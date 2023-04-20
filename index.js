31c65a21a477d064823f98634b17a7ff0d15da0a
Zachary Mowatt <zamowatt@gmail.com>
2023-04-20 11:39:03 -0400

    Arrys lab complete

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Silvester) {
+    cats.push(Silvester);
+}
+
+function destructivelyPrependCat(Benji) {
+    cats.unshift(Benji);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(Spot) {
+    const allCats = [...cats, Spot];
+
+    return allCats;
+}
+
+function prependCat(Milkshake) {
+    const allCats = [Milkshake, ...cats];
+
+    return allCats;
+}
+
+function removeLastCat() {
+    const removeLastCat = cats.slice(0, -1);
+
+    return removeLastCat;
+}
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1);
+
+    return removeFirstCat;
+}
